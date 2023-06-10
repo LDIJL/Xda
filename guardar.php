40e9151ad07c4cd0080ccdf9772e016b365913e8
LDIJL <135484874+LDIJL@users.noreply.github.com>
2023-06-09 21:26:54 -0600

    Update guardar.php

--- guardar.php
+++ guardar.php
@@ -1,11 +1,11 @@
-<<?php
+<?php
 // Establecer la conexión con la base de datos
 $servername = "localhost"; // Cambia esto si tu servidor de base de datos es diferente
 $username = "root"; // Cambia esto si tienes un nombre de usuario diferente
 $password = ""; // Cambia esto si tienes una contraseña diferente
 $dbname = "proyecto1"; // Cambia esto por el nombre de tu base de datos
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli("localhost", "root", "","proyecto1" );
 
 // Verificar la conexión
 if ($conn->connect_error) {
